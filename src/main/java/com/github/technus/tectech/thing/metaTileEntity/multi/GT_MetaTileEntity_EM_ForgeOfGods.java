89d9ad7a29b57b3a400c2f55a0afb75819cb5dde
TheEpicGamer274 <102255081+TheEpicGamer274@users.noreply.github.com>
2023-06-22 23:03:37 -0400

    tooltip upgrade

--- src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_EM_ForgeOfGods.java
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_EM_ForgeOfGods.java
@@ -144,34 +144,6 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
     }
 
     @Override
-    public GT_Multiblock_Tooltip_Builder createTooltip() {
-        final GT_Multiblock_Tooltip_Builder tt = new GT_Multiblock_Tooltip_Builder();
-        tt.addMachineType(translateToLocal("gt.blockmachines.multimachine.em.blackholegenerator.name")) // Machine Type:
-                                                                                                        // Black Hole
-                                                                                                        // Generator
-                .addInfo(translateToLocal("gt.blockmachines.multimachine.em.blackholegenerator.desc.0")) // Controller
-                                                                                                         // block of the
-                                                                                                         // Black Hole
-                                                                                                         // Generator
-                .addInfo(translateToLocal("gt.blockmachines.multimachine.em.blackholegenerator.desc.1")) // Uses a black
-                                                                                                         // hole to
-                                                                                                         // generate
-                                                                                                         // power
-                .addInfo(translateToLocal("tt.keyword.Structure.StructureTooComplex")) // The structure is too complex!
-                .addSeparator().beginStructureBlock(33, 33, 33, false)
-                .addEnergyHatch(translateToLocal("tt.keyword.Structure.AnyHighPowerCasing1D"), 1) // Energy Hatch: Any
-                                                                                                  // High Power Casing
-                                                                                                  // with 1 dot
-                .addMaintenanceHatch(translateToLocal("tt.keyword.Structure.AnyHighPowerCasing1D"), 1) // Maintenance
-                                                                                                       // Hatch: Any
-                                                                                                       // High Power
-                                                                                                       // Casing with 1
-                                                                                                       // dot
-                .toolTipFinisher(CommonValues.TEC_MARK_EM);
-        return tt;
-    }
-
-    @Override
     @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister aBlockIconRegister) {
         ScreenOFF = new Textures.BlockIcons.CustomIcon("iconsets/EM_BHG");
@@ -181,12 +153,12 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
 
     @Override
     public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, ForgeDirection side, ForgeDirection facing,
-            int colorIndex, boolean aActive, boolean aRedstone) {
+                                 int colorIndex, boolean aActive, boolean aRedstone) {
         if (side == facing) {
-            return new ITexture[] { Textures.BlockIcons.casingTexturePages[texturePage][12],
-                    new TT_RenderedExtendedFacingTexture(aActive ? ScreenON : ScreenOFF) };
+            return new ITexture[]{Textures.BlockIcons.casingTexturePages[texturePage][12],
+                    new TT_RenderedExtendedFacingTexture(aActive ? ScreenON : ScreenOFF)};
         }
-        return new ITexture[] { Textures.BlockIcons.casingTexturePages[texturePage][12] };
+        return new ITexture[]{Textures.BlockIcons.casingTexturePages[texturePage][12]};
     }
 
     @Override
@@ -198,10 +170,10 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
 
     // Fuel consumption parameter localisation
     private static final INameFunction<GT_MetaTileEntity_EM_ForgeOfGods> FUEL_CONSUMPTION_PARAM_NAME = (base,
-            p) -> translateToLocal("gt.blockmachines.multimachine.FOG.fuelconsumption");
+                                                                                                        p) -> translateToLocal("gt.blockmachines.multimachine.FOG.fuelconsumption");
     // Fuel consumption parameter value
     private static final IStatusFunction<GT_MetaTileEntity_EM_ForgeOfGods> FUEL_CONSUMPTION_VALUE = (base,
-            p) -> LedStatus.fromLimitsInclusiveOuterBoundary(p.get(), 0, 0, 10, 10);
+                                                                                                     p) -> LedStatus.fromLimitsInclusiveOuterBoundary(p.get(), 0, 0, 10, 10);
 
     @Override
     public void construct(ItemStack stackSize, boolean hintsOnly) {
@@ -383,7 +355,6 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
     public enum moduleElement implements IHatchElement<GT_MetaTileEntity_EM_ForgeOfGods> {
 
         Module(GT_MetaTileEntity_EM_ForgeOfGods::addModuleToMachineList, GT_MetaTileEntity_EM_BaseModule.class) {
-
             @Override
             public long count(GT_MetaTileEntity_EM_ForgeOfGods tileEntity) {
                 return tileEntity.moduleHatches.size();
@@ -395,7 +366,7 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
 
         @SafeVarargs
         moduleElement(IGT_HatchAdder<GT_MetaTileEntity_EM_ForgeOfGods> adder,
-                Class<? extends IMetaTileEntity>... mteClasses) {
+                      Class<? extends IMetaTileEntity>... mteClasses) {
             this.mteClasses = Collections.unmodifiableList(Arrays.asList(mteClasses));
             this.adder = adder;
         }
@@ -475,6 +446,25 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
     @Override
     public GT_Recipe.GT_Recipe_Map getRecipeMap() {
         return GT_Recipe.GT_Recipe_Map.sBlastRecipes;
+    }
+
+
+    @Override
+    public GT_Multiblock_Tooltip_Builder createTooltip() {
+        final GT_Multiblock_Tooltip_Builder tt = new GT_Multiblock_Tooltip_Builder();
+        tt.addMachineType("Material Manipulator") // Machine Type:
+                .addInfo("Controller block for the Forge of Gods") // Controller
+                .addInfo("Uses a Star to to manipulate metals")
+                .addSeparator().beginStructureBlock(1, 4, 2, false)
+                .addStructureInfo("Output bus/hatch has to be the ME variant")
+                .addStructureInfo("Dot 2 of Input Hatch is the Fuel Input Hatch")
+                .addInputHatch("Any Infinite Spacetime Casing", 1)
+                .addInputHatch("Any Infinite Spacetime Casing", 2) // Fuel Input Hatch
+                .addInputBus("Any Infinite Spacetime Casing",1)
+                .addOutputBus("Any Infinite Spacetime Casing",1)
+                .addOutputHatch("Any Infinite Spacetime Casing",1)
+                .toolTipFinisher(CommonValues.TEC_MARK_EM);
+        return tt;
     }
 
     @Override
