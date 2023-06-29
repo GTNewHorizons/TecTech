e8f1a195a1d06bf362e8799e6588659947b938f5
GDCloud <gdcloudstrike@gmail.com>
2023-06-29 02:27:36 +0200

    first recipe addition loops

--- src/main/java/com/github/technus/tectech/loader/recipe/ForgeOfGods.java
+++ src/main/java/com/github/technus/tectech/loader/recipe/ForgeOfGods.java
@@ -14,16 +14,29 @@ public class ForgeOfGods implements Runnable {
 
     @Override
     public void run() {
-        TT_recipeAdder.addFOGPlasmaRecipe(
-                new ItemStack[] { Materials.Tritanium.getDust(1) },
-                new FluidStack[] { Materials.Tritanium.getPlasma(144) },
-                1 * SECONDS,
-                (int) TierEU.RECIPE_MAX);
 
-        TT_recipeAdder.addFOGPlasmaRecipe(
-                new FluidStack[] { Materials.Helium.getGas(1000) },
-                new FluidStack[] { Materials.Helium.getPlasma(1000) },
-                1 * SECONDS,
-                (int) TierEU.RECIPE_MAX);
+        // GT solid to plasma recipes
+        ItemStack[] solids = { Materials.Tritanium.getDust(1), Materials.Aluminium.getDust(1)};
+        FluidStack[] solid_plasmas = { Materials.Tritanium.getPlasma(144), Materials.Aluminium.getPlasma(144)};
+
+        for (int i = 0; i < solids.length; i++) {
+            TT_recipeAdder.addFOGPlasmaRecipe(
+                    new ItemStack[] { solids[i] },
+                    new FluidStack[] { solid_plasmas[i] },
+                    1 * SECONDS,
+                    (int) TierEU.RECIPE_MAX);
+        }
+
+        // GT fluid to plasma recipes
+        FluidStack[] fluids = { Materials.Helium.getGas(1000)};
+        FluidStack[] fluid_plasmas = { Materials.Helium.getPlasma(1000)};
+
+        for (int i = 0; i < fluids.length; i++) {
+            TT_recipeAdder.addFOGPlasmaRecipe(
+                    new FluidStack[] { fluids[i] },
+                    new FluidStack[] { fluid_plasmas[i] },
+                    1 * SECONDS,
+                    (int) TierEU.RECIPE_MAX);
+        }
     }
 }
