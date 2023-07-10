9945506539b290d29e6610318ec7355c6a14ae03
GDCloud <gdcloudstrike@gmail.com>
2023-07-10 03:44:24 +0200

    add rendering block

--- src/main/java/com/github/technus/tectech/thing/casing/TT_Container_Casings.java
+++ src/main/java/com/github/technus/tectech/thing/casing/TT_Container_Casings.java
@@ -3,6 +3,7 @@ package com.github.technus.tectech.thing.casing;
 import net.minecraft.block.Block;
 
 import com.github.technus.tectech.thing.block.EyeOfHarmonyBlock;
+import com.github.technus.tectech.thing.block.ForgeOfGodsBlock;
 
 /**
  * Created by danie_000 on 03.10.2016.
@@ -19,6 +20,7 @@ public final class TT_Container_Casings {
     public static Block StabilisationFieldGenerators;
 
     public static Block eyeOfHarmonyRenderBlock = new EyeOfHarmonyBlock();
+    public static Block forgeOfGodsRenderBlock = new ForgeOfGodsBlock();
 
     public static Block GravitonFlowModulators;
 
