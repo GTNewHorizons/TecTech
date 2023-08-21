344250e357791c523ae3ca2f2921cdf28a1cb4fb
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-08-21 13:54:00 +0100

    Processing logic

---
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/em_machine/StarConsumerProcessingLogic.java
@@ -0,0 +1,38 @@
+package com.github.technus.tectech.thing.metaTileEntity.multi.em_machine;
+
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.logic.ProcessingLogic;
+import gregtech.api.recipe.check.CheckRecipeResult;
+import gregtech.api.recipe.check.CheckRecipeResultRegistry;
+import gregtech.api.util.GT_OreDictUnificator;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class StarConsumerProcessingLogic extends ProcessingLogic {
+
+    @Override
+    public @Nonnull CheckRecipeResult process() {
+        if (inputItems == null || inputItems[0] == null) {
+            return CheckRecipeResultRegistry.NO_RECIPE;
+        }
+
+
+        ItemStack celestialBody = inputItems[0];
+
+//        List<ItemStack> output = new ArrayList<ItemStack>();
+//        output.add(GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.Europium, 2));
+//        output.add(GT_OreDictUnificator.get(OrePrefixes.plate, Materials.Europium, 2));
+
+        celestialBody.stackSize -= 1;
+        setDuration(1);
+
+        setOutputItems(GT_OreDictUnificator.get(OrePrefixes.plateDense, Materials.Europium, 2), GT_OreDictUnificator.get(OrePrefixes.plate, Materials.Europium, 2));
+
+        return CheckRecipeResultRegistry.SUCCESSFUL;
+    }
+
+}
