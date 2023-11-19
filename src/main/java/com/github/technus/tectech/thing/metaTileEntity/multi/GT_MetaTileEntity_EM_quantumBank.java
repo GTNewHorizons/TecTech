626222fe70710d2422c5eef894493056e50bde81
SMSum <smsdevelopment@gmail.com>
2023-11-18 18:45:09 -0600

    GUI rework, need to fix setTexts() or remove recipe checking entirely. Useless in multi.

--- src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_EM_quantumBank.java
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_EM_quantumBank.java
@@ -8,14 +8,20 @@ import static com.gtnewhorizon.structurelib.structure.StructureUtility.transpose
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlockUnlocalizedName;
 
 import static gregtech.api.enums.GT_HatchElement.InputHatch;
+import static gregtech.api.metatileentity.BaseTileEntity.TOOLTIP_DELAY;
 import static gregtech.api.util.GT_StructureUtility.buildHatchAdder;
 import static gregtech.api.util.GT_StructureUtility.ofFrame;
 import static net.minecraft.util.StatCollector.translateToLocal;
 
 
+import com.github.technus.tectech.TecTech;
 import com.github.technus.tectech.thing.gui.TecTechUITextures;
+import com.gtnewhorizons.modularui.api.ModularUITextures;
+import com.gtnewhorizons.modularui.api.drawable.IDrawable;
+import com.gtnewhorizons.modularui.api.drawable.UITexture;
 import com.gtnewhorizons.modularui.api.forge.ItemStackHandler;
 import com.gtnewhorizons.modularui.api.math.Pos2d;
+import com.gtnewhorizons.modularui.api.math.Size;
 import com.gtnewhorizons.modularui.api.screen.ModularWindow;
 import com.gtnewhorizons.modularui.api.screen.UIBuildContext;
 import com.gtnewhorizons.modularui.api.widget.Widget;
@@ -23,6 +29,7 @@ import com.gtnewhorizons.modularui.common.internal.wrapper.BaseSlot;
 import com.gtnewhorizons.modularui.common.widget.ButtonWidget;
 import com.gtnewhorizons.modularui.common.widget.ChangeableWidget;
 import com.gtnewhorizons.modularui.common.widget.DrawableWidget;
+import com.gtnewhorizons.modularui.common.widget.DynamicPositionedColumn;
 import com.gtnewhorizons.modularui.common.widget.DynamicPositionedRow;
 import com.gtnewhorizons.modularui.common.widget.FakeSyncWidget;
 import com.gtnewhorizons.modularui.common.widget.Scrollable;
@@ -60,6 +67,7 @@ import gregtech.api.util.GT_Multiblock_Tooltip_Builder;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GT_MetaTileEntity_EM_quantumBank extends GT_MetaTileEntity_MultiblockBase_EM
         implements ISurvivalConstructable {
@@ -204,23 +212,74 @@ public class GT_MetaTileEntity_EM_quantumBank extends GT_MetaTileEntity_Multiblo
     public void addGregTechLogo(ModularWindow.Builder builder) { //Moving this stupid ass logo in the way
         builder.widget(
                 new DrawableWidget().setDrawable(TecTechUITextures.PICTURE_TECTECH_LOGO_DARK).setSize(18, 18)
-                        .setPos(173, 168));
+                        .setPos(173, 184));
     }
-    /*
+    @Override
+    protected ButtonWidget createPowerSwitchButton() { //We have to override this because .setPos on our inherited clashes since its predefined :fail:
+        Widget button = new ButtonWidget().setOnClick((clickData, widget) -> {
+            if (isAllowedToWorkButtonEnabled()) {
+                TecTech.proxy.playSound(getBaseMetaTileEntity(), "fx_click");
+                if (getBaseMetaTileEntity().isAllowedToWork()) {
+                    getBaseMetaTileEntity().disableWorking();
+                } else {
+                    getBaseMetaTileEntity().enableWorking();
+                }
+            }
+        }).setPlayClickSound(false).setBackground(() -> {
+            List<UITexture> ret = new ArrayList<>();
+            ret.add(TecTechUITextures.BUTTON_STANDARD_16x16);
+            if (!isAllowedToWorkButtonEnabled()) {
+                ret.add(TecTechUITextures.OVERLAY_BUTTON_POWER_SWITCH_DISABLED);
+            } else {
+                if (getBaseMetaTileEntity().isAllowedToWork()) {
+                    ret.add(TecTechUITextures.OVERLAY_BUTTON_POWER_SWITCH_ON);
+                } else {
+                    ret.add(TecTechUITextures.OVERLAY_BUTTON_POWER_SWITCH_OFF);
+                }
+            }
+            return ret.toArray(new IDrawable[0]);
+        }).setPos(174,242).setSize(16, 16);
+        if (isAllowedToWorkButtonEnabled()) {
+            button.addTooltip("Power Switch").setTooltipShowUpDelay(TOOLTIP_DELAY);
+        }
+        return (ButtonWidget) button;
+    }
+
     @Override
     public void bindPlayerInventoryUI(ModularWindow.Builder builder, UIBuildContext buildContext) {
         builder.bindPlayerInventory(
                 buildContext.getPlayer(),
-                new Pos2d(7, 133), //Inventory POS (PLAYERS)
+                new Pos2d(7, 183), //Inventory POS (PLAYERS)
                 this.getGUITextureSet()
                         .getItemSlot());
     }
-     */
+    private DynamicPositionedColumn drawTexts(DynamicPositionedColumn screenElements){
+        screenElements.setSynced(false)
+                .setSpace(0)
+                .setPos(10,7);
+        screenElements.widget(new TextWidget("§aProviding Data"));
+        screenElements.widget(new TextWidget("Number of Data Sticks: " + 1)
+                .setDefaultColor(COLOR_TEXT_WHITE.get()));
+        screenElements.widget(new TextWidget("Current Capacity: " + inventoryHandler.getSlots())
+                .setDefaultColor(COLOR_TEXT_WHITE.get()));
+        return screenElements;
+    }
     @Override
     public void addUIWidgets(ModularWindow.Builder builder, UIBuildContext buildContext) {
+        final DynamicPositionedColumn screenElements = new DynamicPositionedColumn();
+        builder.setSize(new Size(200, 265));
+        builder.setBackground(ModularUITextures.VANILLA_BACKGROUND);
         builder.widget(
                 new DrawableWidget().setDrawable(TecTechUITextures.BACKGROUND_SCREEN_BLUE).setPos(4, 4)
                         .setSize(190, 91));
+        builder.widget(
+                new DrawableWidget().setDrawable(TecTechUITextures.BACKGROUND_SCREEN_BLUE).setPos(4, 98)
+                        .setSize(190, 81));
+        drawTexts(screenElements);
+        builder.widget(screenElements.setPos(7, 8));
+
+
+
 
         Widget powerSwitchButton = createPowerSwitchButton();
         builder.widget(powerSwitchButton)
@@ -228,6 +287,7 @@ public class GT_MetaTileEntity_EM_quantumBank extends GT_MetaTileEntity_Multiblo
                     if (val) getBaseMetaTileEntity().enableWorking();
                     else getBaseMetaTileEntity().disableWorking();
                 }));
+
         final Scrollable scrollable = new Scrollable().setVerticalScroll();
         for (int row = 0; row * 10 < cappedStorage - 1; row++) {
             int columnsToMake = Math.min(cappedStorage - row * 10, 10);
@@ -237,7 +297,7 @@ public class GT_MetaTileEntity_EM_quantumBank extends GT_MetaTileEntity_Multiblo
                                 .setSize(18, 18));
             }
         }
-        builder.widget(scrollable.setSize(18 * 10 + 4, 18 * 4).setPos(7, 16));
+        builder.widget(scrollable.setSize(18 * 10 + 4, 18 * 4).setPos(7, 103));
 
     }
 
