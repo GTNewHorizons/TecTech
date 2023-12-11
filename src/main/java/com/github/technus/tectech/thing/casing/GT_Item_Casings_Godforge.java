cc2f27eeea55f86f54a611cffbde42c3c32d7e31
GDCloud <gdcloudstrike@gmail.com>
2023-12-11 02:50:09 +0100

    more tooltip stuff

--- src/main/java/com/github/technus/tectech/thing/casing/GT_Item_Casings_Godforge.java
+++ src/main/java/com/github/technus/tectech/thing/casing/GT_Item_Casings_Godforge.java
@@ -2,6 +2,8 @@ package com.github.technus.tectech.thing.casing;
 
 import static com.github.technus.tectech.util.CommonValues.GODFORGE_MARK;
 import static gregtech.api.enums.GT_Values.AuthorColen;
+import static net.minecraft.util.EnumChatFormatting.AQUA;
+import static net.minecraft.util.EnumChatFormatting.BOLD;
 
 import java.util.List;
 
@@ -24,24 +26,84 @@ public class GT_Item_Casings_Godforge extends GT_Item_Casings_Abstract {
         aList.add(GODFORGE_MARK);
         switch (aStack.getItemDamage()) {
             case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
                 aList.add(
-                        EnumChatFormatting.AQUA.toString() + EnumChatFormatting.BOLD
+                        GT_LanguageManager.addStringLocalization(
+                                "godforge.casings.0.Tooltip.0",
+                                "Shielded by the event horizon of a singularity"));
+                aList.add(
+                        AQUA.toString() + BOLD
+                                + GT_LanguageManager.addStringLocalization(
+                                        "godforge.casings.0.Tooltip.1",
+                                        "Don't get too close..."));
+                break;
+            case 1:
+                aList.add(
+                        GT_LanguageManager.addStringLocalization(
+                                "godforge.casings.1.Tooltip.0",
+                                "Designed to route stellar matter using spacetime distortion"));
+                aList.add(
+                        AQUA.toString() + BOLD
                                 + GT_LanguageManager
-                                        .addStringLocalization("FOG.GravitonModulation.Standard.Tooltip", "basel."));
+                                        .addStringLocalization("godforge.casings.1.Tooltip.1", "Reality Distortion"));
+                break;
+            case 2:
+                aList.add(
+                        GT_LanguageManager.addStringLocalization(
+                                "godforge.casings.2.Tooltip.0",
+                                "Unaffected by gravitational forces"));
+                aList.add(
+                        AQUA.toString() + BOLD
+                                + GT_LanguageManager.addStringLocalization(
+                                        "godforge.casings.2.Tooltip.1",
+                                        "Not even a black hole could tear it apart."));
+                break;
+            case 3:
+                aList.add(
+                        GT_LanguageManager.addStringLocalization(
+                                "godforge.casings.3.Tooltip.0",
+                                "Creates enormous magnetic fields capable of constraining entire stars"));
+                aList.add(
+                        AQUA.toString() + BOLD
+                                + GT_LanguageManager.addStringLocalization(
+                                        "godforge.casings.3.Tooltip.1",
+                                        "It's super effective!"));
+                break;
+            case 4:
+                aList.add(
+                        GT_LanguageManager.addStringLocalization(
+                                "godforge.casings.4.Tooltip.0",
+                                "Controls the flow of gravitons to manipulate gravity"));
+                aList.add(
+                        AQUA.toString() + BOLD
+                                + GT_LanguageManager.addStringLocalization(
+                                        "godforge.casings.4.Tooltip.1",
+                                        "Exponential scaling past 800 Galaxies"));
+                break;
+            case 5:
+                aList.add(
+                        GT_LanguageManager.addStringLocalization(
+                                "godforge.casings.5.Tooltip.0",
+                                "Controls the flow of gravitons to manipulate gravity"));
+                aList.add(
+                        AQUA.toString() + BOLD
+                                + GT_LanguageManager
+                                        .addStringLocalization("godforge.casings.5.Tooltip.1", "Getting closer..."));
+                break;
+            case 6:
+                aList.add(
+                        GT_LanguageManager.addStringLocalization(
+                                "godforge.casings.6.Tooltip.0",
+                                "Controls the flow of gravitons to manipulate gravity"));
+                aList.add(
+                        AQUA.toString() + BOLD
+                                + GT_LanguageManager
+                                        .addStringLocalization("godforge.casings.6.Tooltip.1", "Gravity Central"));
                 break;
             default:
                 aList.add(
-                        EnumChatFormatting.RED.toString() + EnumChatFormatting.BOLD
+                        EnumChatFormatting.RED.toString() + BOLD
                                 + GT_LanguageManager.addStringLocalization(
-                                        "FOG.GravitonModulation.Error.Tooltip",
+                                        "godforge.casings.Error.Tooltip",
                                         "Error, report to GTNH team"));
         }
         aList.add(AuthorColen);
