a6246df33c2e76f95d05bacc8f8e6fb8c6247a4d
GDCloud <gdcloudstrike@gmail.com>
2024-02-24 21:10:17 +0100

    implement system to restrict the order which upgrades can be bought in

--- src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_EM_ForgeOfGods.java
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/GT_MetaTileEntity_EM_ForgeOfGods.java
@@ -81,7 +81,7 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
     private String userUUID = "";
     protected static final String STRUCTURE_PIECE_MAIN = "main";
 
-    public HashMap<Boolean, Integer> upgradeMap = new HashMap<>();
+    private Boolean debugMode = true;
 
     public int survivalConstruct(ItemStack stackSize, int elementBudget, IItemSource source, EntityPlayerMP actor) {
         if (mMachine) return -1;
@@ -527,6 +527,10 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
 
     private int currentUpgradeID = 0;
     private int currentColorCode = 0;
+    private int[] prereqUpgrades = new int[] {};
+    private int[] followupUpgrades = new int[] {};
+    private boolean allPrereqRequired = false;
+    private boolean isUpradeSplitStart = false;
     public boolean[] upgrades = new boolean[31];
 
     protected ModularWindow createUpgradeTreeWindow(final EntityPlayer player) {
@@ -534,36 +538,93 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
         final int PARENT_WIDTH = 300;
         final int PARENT_HEIGHT = 1000;
         ModularWindow.Builder builder = ModularWindow.builder(PARENT_WIDTH, PARENT_HEIGHT);
-        scrollable.widget(createUpgradeBox(0, 0, new Pos2d(126, 56)))
-                .widget(createUpgradeBox(1, 0, new Pos2d(126, 116))).widget(createUpgradeBox(2, 0, new Pos2d(96, 176)))
-                .widget(createUpgradeBox(3, 0, new Pos2d(156, 176))).widget(createUpgradeBox(4, 0, new Pos2d(66, 236)))
-                .widget(createUpgradeBox(5, 0, new Pos2d(126, 236))).widget(createUpgradeBox(6, 0, new Pos2d(186, 236)))
-                .widget(createUpgradeBox(7, 0, new Pos2d(126, 296))).widget(createUpgradeBox(8, 0, new Pos2d(56, 356)))
-                .widget(createUpgradeBox(9, 0, new Pos2d(126, 356)))
-                .widget(createUpgradeBox(10, 0, new Pos2d(196, 356)))
-                .widget(createUpgradeBox(11, 0, new Pos2d(126, 416)))
-                .widget(createUpgradeBox(12, 1, new Pos2d(66, 476)))
-                .widget(createUpgradeBox(13, 2, new Pos2d(126, 476)))
-                .widget(createUpgradeBox(14, 3, new Pos2d(186, 476)))
-                .widget(createUpgradeBox(15, 3, new Pos2d(246, 496))).widget(createUpgradeBox(16, 1, new Pos2d(6, 556)))
-                .widget(createUpgradeBox(17, 1, new Pos2d(66, 536)))
-                .widget(createUpgradeBox(18, 2, new Pos2d(126, 536)))
-                .widget(createUpgradeBox(19, 3, new Pos2d(186, 536)))
-                .widget(createUpgradeBox(20, 1, new Pos2d(66, 596)))
-                .widget(createUpgradeBox(21, 2, new Pos2d(126, 596)))
-                .widget(createUpgradeBox(22, 3, new Pos2d(186, 596)))
-                .widget(createUpgradeBox(23, 0, new Pos2d(126, 656)))
-                .widget(createUpgradeBox(24, 0, new Pos2d(126, 718)))
-                .widget(createUpgradeBox(25, 0, new Pos2d(36, 758))).widget(createUpgradeBox(26, 0, new Pos2d(36, 848)))
-                .widget(createUpgradeBox(27, 0, new Pos2d(126, 888)))
-                .widget(createUpgradeBox(28, 0, new Pos2d(216, 848)))
-                .widget(createUpgradeBox(29, 0, new Pos2d(216, 758)))
-                .widget(createUpgradeBox(30, 0, new Pos2d(126, 798))).widget(new TextWidget("").setPos(0, 1000));
+        scrollable.widget(createUpgradeBox(0, 0, new int[] {}, false, new int[] { 1 }, false, new Pos2d(126, 56)))
+                .widget(createUpgradeBox(1, 0, new int[] { 0 }, false, new int[] { 2, 3 }, false, new Pos2d(126, 116)))
+                .widget(createUpgradeBox(2, 0, new int[] { 1 }, false, new int[] { 4, 5 }, false, new Pos2d(96, 176)))
+                .widget(createUpgradeBox(3, 0, new int[] { 1 }, false, new int[] { 5, 6 }, false, new Pos2d(156, 176)))
+                .widget(createUpgradeBox(4, 0, new int[] { 2 }, false, new int[] { 8 }, false, new Pos2d(66, 236)))
+                .widget(createUpgradeBox(5, 0, new int[] { 2, 3 }, false, new int[] { 7 }, false, new Pos2d(126, 236)))
+                .widget(createUpgradeBox(6, 0, new int[] { 3 }, false, new int[] { 10 }, false, new Pos2d(186, 236)))
+                .widget(
+                        createUpgradeBox(
+                                7,
+                                0,
+                                new int[] { 5 },
+                                false,
+                                new int[] { 8, 9, 10 },
+                                false,
+                                new Pos2d(126, 296)))
+                .widget(createUpgradeBox(8, 0, new int[] { 4, 7 }, true, new int[] { 11 }, false, new Pos2d(56, 356)))
+                .widget(createUpgradeBox(9, 0, new int[] { 7 }, false, new int[] {}, false, new Pos2d(126, 356)))
+                .widget(createUpgradeBox(10, 0, new int[] { 6, 7 }, true, new int[] { 11 }, false, new Pos2d(196, 356)))
+                .widget(
+                        createUpgradeBox(
+                                11,
+                                0,
+                                new int[] { 8, 10 },
+                                false,
+                                new int[] { 12, 13, 14 },
+                                false,
+                                new Pos2d(126, 416)))
+                .widget(createUpgradeBox(12, 1, new int[] { 11 }, false, new int[] { 17 }, true, new Pos2d(66, 476)))
+                .widget(createUpgradeBox(13, 2, new int[] { 11 }, false, new int[] { 18 }, true, new Pos2d(126, 476)))
+                .widget(
+                        createUpgradeBox(
+                                14,
+                                3,
+                                new int[] { 11 },
+                                false,
+                                new int[] { 15, 19 },
+                                true,
+                                new Pos2d(186, 476)))
+                .widget(createUpgradeBox(15, 3, new int[] { 14 }, false, new int[] {}, false, new Pos2d(246, 496)))
+                .widget(createUpgradeBox(16, 1, new int[] { 17 }, false, new int[] {}, false, new Pos2d(6, 556)))
+                .widget(
+                        createUpgradeBox(
+                                17,
+                                1,
+                                new int[] { 12 },
+                                false,
+                                new int[] { 16, 20 },
+                                false,
+                                new Pos2d(66, 536)))
+                .widget(createUpgradeBox(18, 2, new int[] { 13 }, false, new int[] { 21 }, false, new Pos2d(126, 536)))
+                .widget(createUpgradeBox(19, 3, new int[] { 14 }, false, new int[] { 22 }, false, new Pos2d(186, 536)))
+                .widget(createUpgradeBox(20, 1, new int[] { 17 }, false, new int[] { 23 }, false, new Pos2d(66, 596)))
+                .widget(createUpgradeBox(21, 2, new int[] { 18 }, false, new int[] { 23 }, false, new Pos2d(126, 596)))
+                .widget(createUpgradeBox(22, 3, new int[] { 19 }, false, new int[] { 23 }, false, new Pos2d(186, 596)))
+                .widget(
+                        createUpgradeBox(
+                                23,
+                                0,
+                                new int[] { 20, 21, 22 },
+                                false,
+                                new int[] { 24 },
+                                false,
+                                new Pos2d(126, 656)))
+                .widget(createUpgradeBox(24, 0, new int[] { 23 }, false, new int[] { 25 }, false, new Pos2d(126, 718)))
+                .widget(createUpgradeBox(25, 0, new int[] { 24 }, false, new int[] { 26 }, false, new Pos2d(36, 758)))
+                .widget(createUpgradeBox(26, 0, new int[] { 25 }, false, new int[] { 27 }, false, new Pos2d(36, 848)))
+                .widget(createUpgradeBox(27, 0, new int[] { 26 }, false, new int[] { 28 }, false, new Pos2d(126, 888)))
+                .widget(createUpgradeBox(28, 0, new int[] { 27 }, false, new int[] { 29 }, false, new Pos2d(216, 848)))
+                .widget(createUpgradeBox(29, 0, new int[] { 28 }, false, new int[] { 30 }, false, new Pos2d(216, 758)))
+                .widget(createUpgradeBox(30, 0, new int[] { 29 }, false, new int[] {}, false, new Pos2d(126, 798)))
+                .widget(new TextWidget("").setPos(0, 1000));
 
         builder.widget(
                 new DrawableWidget().setDrawable(TecTechUITextures.BACKGROUND_STAR).setPos(0, 350).setSize(300, 300))
-                .widget(ButtonWidget.closeWindowButton(true).setPos(284, 4))
-                .widget(scrollable.setSize(292, 292).setPos(4, 354));
+                .widget(scrollable.setSize(292, 292).setPos(4, 354))
+                .widget(ButtonWidget.closeWindowButton(true).setPos(282, 354));
+        if (debugMode) {
+            builder.widget(
+                    new MultiChildWidget().addChild(
+                            new ButtonWidget().setOnClick((clickData, widget) -> { upgrades = new boolean[31]; })
+                                    .setSize(40, 15).setBackground(GT_UITextures.BUTTON_STANDARD))
+                            .addChild(
+                                    new TextWidget("Reset").setTextAlignment(Alignment.Center).setScale(0.57f)
+                                            .setMaxWidth(36).setPos(3, 3))
+                            .setPos(4, 354));
+        }
         return builder.build();
     }
 
@@ -588,22 +649,65 @@ public class GT_MetaTileEntity_EM_ForgeOfGods extends GT_MetaTileEntity_Multiblo
                                                 .setDefaultColor(0x9c9c9c).setPos(9, 110))
                                 .setSize(200, 200))
                 .widget(new MultiChildWidget().addChild(new ButtonWidget().setOnClick((clickData, widget) -> {
-                    upgrades[currentUpgradeID] = !upgrades[currentUpgradeID];
-                    if (!widget.isClient()) {
-                        widget.getContext().openSyncedWindow(11);
+                    int unlockedPrereqUpgrades = 0;
+                    int unlockedFollowupUpgrades = 0;
+                    if (!upgrades[currentUpgradeID]) {
+                        for (int prereqUpgrade : prereqUpgrades) {
+                            if (upgrades[prereqUpgrade]) {
+                                unlockedPrereqUpgrades++;
+                            }
+                        }
+                        if (allPrereqRequired) {
+                            if (unlockedPrereqUpgrades == prereqUpgrades.length) {
+                                upgrades[currentUpgradeID] = true;
+                            }
+                        } else if (unlockedPrereqUpgrades > 0 || prereqUpgrades.length == 0) {
+                            upgrades[currentUpgradeID] = true;
+                        }
+                    } else {
+                        for (int followupUpgrade : followupUpgrades) {
+                            if (upgrades[followupUpgrade]) {
+                                unlockedFollowupUpgrades++;
+                            }
+                        }
+                        if (unlockedFollowupUpgrades == 0) {
+                            upgrades[currentUpgradeID] = false;
+                        }
                     }
-                }).setSize(40, 15).setBackground(GT_UITextures.BUTTON_STANDARD)
-                        .addTooltip(translateToLocal("fog.upgrade.confirm")).setPos(79, 177)).addChild(
+                }).setSize(40, 15).setBackground(() -> {
+                    if (upgrades[currentUpgradeID]) {
+                        return new IDrawable[] { GT_UITextures.BUTTON_STANDARD_PRESSED };
+                    } else {
+                        return new IDrawable[] { GT_UITextures.BUTTON_STANDARD };
+                    }
+                }).addTooltip(translateToLocal("fog.upgrade.confirm")))
+                        .addChild(
                                 new TextWidget(translateToLocal("fog.upgrade.confirm"))
-                                        .setTextAlignment(Alignment.Center).setScale(0.7f).setMaxWidth(36)
-                                        .setPos(82, 182)));
+                                        .setTextAlignment(Alignment.Center).setScale(0.7f).setMaxWidth(36).setPos(3, 5))
+                        .setPos(79, 177));
         return builder.build();
     }
 
-    private Widget createUpgradeBox(int upgradeID, int colorCode, Pos2d pos) {
+    /**
+     * @param upgradeID               ID of the upgrade
+     * @param colorCode               Number deciding which colored background to use, 0 for blue, 1 for purple, 2 for
+     *                                orange and 3 for green
+     * @param prerequisiteUpgradeIDs  IDs of the prior upgrades directly connected to the current one
+     * @param requireAllPrerequisites Decides how many connected prerequisite upgrades have to be unlocked to be able to
+     *                                unlock this one. True means ALL, False means AT LEAST ONE
+     * @param followingUpgradeIDs     IDs of the following upgrades directly connected to the current one
+     * @param isStartOfSplit          Whether this upgrade is one of the initial split upgrades
+     * @param pos                     Position of the upgrade inside the scrollableWidget
+     */
+    private Widget createUpgradeBox(int upgradeID, int colorCode, int[] prerequisiteUpgradeIDs,
+            boolean requireAllPrerequisites, int[] followingUpgradeIDs, boolean isStartOfSplit, Pos2d pos) {
         return new MultiChildWidget().addChild(new ButtonWidget().setOnClick((clickData, widget) -> {
             currentUpgradeID = upgradeID;
             currentColorCode = colorCode;
+            prereqUpgrades = prerequisiteUpgradeIDs;
+            allPrereqRequired = requireAllPrerequisites;
+            followupUpgrades = followingUpgradeIDs;
+            isUpradeSplitStart = isStartOfSplit;
             if (!widget.isClient()) widget.getContext().openSyncedWindow(11);
         }).setSize(40, 15).setBackground(GT_UITextures.BUTTON_STANDARD)
                 .addTooltip(translateToLocal("fog.upgrade.tt." + upgradeID)))
