073a8b74de2694d1cbb02ab50a4e8fcd7b468777
GDCloud <gdcloudstrike@gmail.com>
2024-03-10 23:42:13 +0100

    remove this so it actually loads

--- src/main/java/com/github/technus/tectech/loader/thing/MuTeLoader.java
+++ src/main/java/com/github/technus/tectech/loader/thing/MuTeLoader.java
@@ -1,13 +1,6 @@
 package com.github.technus.tectech.loader.thing;
 
-import static gregtech.loaders.preload.GT_Loader_MultiTileEntities.*;
-import static gregtech.loaders.preload.GT_Loader_MultiTileEntities.CASING_BLOCK;
-
 import com.github.technus.tectech.TecTech;
-import com.github.technus.tectech.thing.multiTileEntity.GodForge;
-
-import gregtech.api.enums.Materials;
-import gregtech.api.multitileentity.multiblock.casing.BasicCasing;
 
 public class MuTeLoader implements Runnable {
 
@@ -19,13 +12,10 @@ public class MuTeLoader implements Runnable {
     }
 
     private static void registerMachines() {
-        MACHINE_REGISTRY.create(5000, GodForge.class).name("Forge of Gods").category("Multiblock Controller")
-                .setBlock(MACHINE_BLOCK).material(Materials.Iron).textureFolder("godForge").tankCapacity(262144000L)
-                .inputInventorySize(64).outputInventorySize(64).register();
+
     }
 
     private static void registerCasings() {
-        CASING_REGISTRY.create(5000, BasicCasing.class).name("Spatially Confined Boundary Casing")
-                .category("MultiBlock Casing").setBlock(CASING_BLOCK).textureFolder("godForge").register();
+
     }
 }
