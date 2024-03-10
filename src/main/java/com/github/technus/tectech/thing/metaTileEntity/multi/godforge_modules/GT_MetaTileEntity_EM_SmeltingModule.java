e4bd4ad40cecc5a69e06e14dac2c7b76b3ed3fb1
GDCloud <gdcloudstrike@gmail.com>
2024-03-11 00:18:49 +0100

    add furnace mode

--- src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/godforge_modules/GT_MetaTileEntity_EM_SmeltingModule.java
+++ src/main/java/com/github/technus/tectech/thing/metaTileEntity/multi/godforge_modules/GT_MetaTileEntity_EM_SmeltingModule.java
@@ -1,6 +1,7 @@
 package com.github.technus.tectech.thing.metaTileEntity.multi.godforge_modules;
 
 import static com.github.technus.tectech.thing.casing.GT_Block_CasingsTT.texturePage;
+import static gregtech.api.metatileentity.BaseTileEntity.TOOLTIP_DELAY;
 import static gregtech.api.util.GT_Utility.formatNumbers;
 import static gregtech.common.misc.WirelessNetworkManager.addEUToGlobalEnergyMap;
 import static gregtech.common.misc.WirelessNetworkManager.getUserEU;
@@ -8,12 +9,19 @@ import static net.minecraft.util.EnumChatFormatting.GREEN;
 import static net.minecraft.util.EnumChatFormatting.RED;
 import static net.minecraft.util.EnumChatFormatting.RESET;
 import static net.minecraft.util.EnumChatFormatting.YELLOW;
+import static net.minecraft.util.StatCollector.translateToLocal;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import org.jetbrains.annotations.NotNull;
@@ -21,13 +29,24 @@ import org.jetbrains.annotations.NotNull;
 import com.github.technus.tectech.thing.metaTileEntity.multi.base.*;
 import com.github.technus.tectech.thing.metaTileEntity.multi.base.render.TT_RenderedExtendedFacingTexture;
 import com.github.technus.tectech.util.CommonValues;
+import com.gtnewhorizons.modularui.api.drawable.IDrawable;
+import com.gtnewhorizons.modularui.api.drawable.UITexture;
+import com.gtnewhorizons.modularui.api.screen.ModularWindow;
+import com.gtnewhorizons.modularui.api.screen.UIBuildContext;
+import com.gtnewhorizons.modularui.api.widget.IWidgetBuilder;
+import com.gtnewhorizons.modularui.api.widget.Widget;
+import com.gtnewhorizons.modularui.common.widget.ButtonWidget;
+import com.gtnewhorizons.modularui.common.widget.FakeSyncWidget;
 
 import gregtech.api.enums.Textures;
 import gregtech.api.enums.TierEU;
+import gregtech.api.gui.modularui.GT_UITextures;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.logic.ProcessingLogic;
+import gregtech.api.recipe.RecipeMap;
+import gregtech.api.recipe.RecipeMaps;
 import gregtech.api.recipe.check.CheckRecipeResult;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
 import gregtech.api.util.*;
@@ -36,6 +55,7 @@ public class GT_MetaTileEntity_EM_SmeltingModule extends GT_MetaTileEntity_EM_Ba
 
     private long EUt = 0;
     private long currentParallel = 0;
+    private boolean furnaceMode = false;
 
     public GT_MetaTileEntity_EM_SmeltingModule(int aID, String aName, String aNameRegional) {
         super(aID, aName, aNameRegional);
@@ -50,11 +70,37 @@ public class GT_MetaTileEntity_EM_SmeltingModule extends GT_MetaTileEntity_EM_Ba
         return new GT_MetaTileEntity_EM_SmeltingModule(mName);
     }
 
+    @Override
+    public RecipeMap<?> getRecipeMap() {
+        return null;
+    }
+
+    @Nonnull
+    @Override
+    public Collection<RecipeMap<?>> getAvailableRecipeMaps() {
+        return Arrays.asList(RecipeMaps.blastFurnaceRecipes, RecipeMaps.furnaceRecipes);
+    }
+
+    private static RecipeMap<?> getRecipeMap(boolean furnaceMode) {
+        if (furnaceMode) {
+            return RecipeMaps.furnaceRecipes;
+        } else {
+            return RecipeMaps.blastFurnaceRecipes;
+        }
+    }
+
     long wirelessEUt = 0;
 
     @Override
     protected ProcessingLogic createProcessingLogic() {
         return new ProcessingLogic() {
+
+            @Nonnull
+            @Override
+            protected Stream<GT_Recipe> findRecipeMatches(@Nullable RecipeMap<?> map) {
+                RecipeMap<?> recipes = getRecipeMap(furnaceMode);
+                return super.findRecipeMatches(recipes);
+            }
 
             @NotNull
             @Override
@@ -111,6 +157,52 @@ public class GT_MetaTileEntity_EM_SmeltingModule extends GT_MetaTileEntity_EM_Ba
                                     : GT_MetaTileEntity_MultiblockBase_EM.ScreenOFF) };
         }
         return new ITexture[] { Textures.BlockIcons.getCasingTextureForId(texturePage << 7) };
+    }
+
+    @Override
+    public void addUIWidgets(ModularWindow.Builder builder, UIBuildContext buildContext) {
+        super.addUIWidgets(builder, buildContext);
+        builder.widget(furnaceSwitch(builder));
+
+    }
+
+    protected ButtonWidget furnaceSwitch(IWidgetBuilder<?> builder) {
+        Widget button = new ButtonWidget().setOnClick((clickData, widget) -> furnaceMode = !furnaceMode)
+                .setPlayClickSound(isFurnaceModeOn()).setBackground(() -> {
+                    List<UITexture> ret = new ArrayList<>();
+                    if (isFurnaceModeOn()) {
+                        ret.add(GT_UITextures.BUTTON_STANDARD_PRESSED);
+                        ret.add(GT_UITextures.OVERLAY_BUTTON_CHECKMARK);
+                    } else {
+                        ret.add(GT_UITextures.BUTTON_STANDARD);
+                        ret.add(GT_UITextures.OVERLAY_BUTTON_CROSS);
+
+                    }
+                    return ret.toArray(new IDrawable[0]);
+                }).attachSyncer(new FakeSyncWidget.BooleanSyncer(this::isFurnaceModeOn, this::setFurnaceMode), builder)
+                .addTooltip(translateToLocal("fog.button.furnacemode.tooltip")).setTooltipShowUpDelay(TOOLTIP_DELAY)
+                .setPos(174, 91).setSize(16, 16);
+        return (ButtonWidget) button;
+    }
+
+    private boolean isFurnaceModeOn() {
+        return furnaceMode;
+    }
+
+    private void setFurnaceMode(boolean enabled) {
+        furnaceMode = enabled;
+    }
+
+    @Override
+    public void saveNBTData(NBTTagCompound NBT) {
+        NBT.setBoolean("furnaceMode", furnaceMode);
+        super.saveNBTData(NBT);
+    }
+
+    @Override
+    public void loadNBTData(final NBTTagCompound NBT) {
+        furnaceMode = NBT.getBoolean("furnaceMode");
+        super.loadNBTData(NBT);
     }
 
     @Override
